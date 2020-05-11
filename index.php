d3c1cb53a945c17ea44367eb12909a6d4269262d
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-11 11:26:58 +0200

    ex5 partie3 php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $departments = array();
-$departments[80] = 'somme';
-$departments[02] = 'aisne';
-$departments[60] = 'oise';
-$departments[62] = 'pas de calais';
-$departments[59] = 'nord';
+$departments['80'] = 'somme';
+$departments['02'] = 'aisne';
+$departments['60'] = 'oise';
+$departments['62'] = 'pas de calais';
+$departments['59'] = 'nord';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -17,6 +17,12 @@ $departments[59] = 'nord';
   foreach ($departments as $department){
     ?>
   <p> <?php echo $department; ?></p>
+<?php } ?>
+
+<?php
+foreach ($departments as $department => $value){  //$value nom du departement en lettre et departement le numéro
+  ?>
+<p> <?php echo $department . ' ' . $value; ?></p><!-- ' ' pour créer des espaces-->
 <?php } ?>
 
 </body>
